23cbcae693a3cb86c30f28d783d75a8011046350
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-15 20:28:46 -0300

    routes for login, register or logou

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const { checkAuth } = require('../controllers/authController');
 const authMiddleware = require('../middleware/auth');
 
 router.get('/check', authMiddleware, checkAuth);
+//There's no need routes for login, register or logout since Supabase handles those for us.
 
 module.exports = router;
