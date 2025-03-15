aa33efe23abb2332bfbe44c8c15ed06c682bd4f0
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-15 20:12:30 -0300

    authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,3 +1,10 @@
 exports.checkAuth = (req, res) => {
-    
+    const {id, email, role} = req.user;
+    res.staus(200).json({
+        user: {
+            id,
+            email, 
+            role: role || 'user'
+        }
+    });
 }
