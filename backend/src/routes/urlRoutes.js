356bef6a9243003e49d2c8663d32e63f941ad298
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-19 22:12:48 -0300

    package

--- backend/src/routes/urlRoutes.js
+++ backend/src/routes/urlRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { convertUrlToPdf } = require('../controllers/urlController');
 const authMiddleware = require('../middleware/auth');
 
-router.post('/convert', authMiddleware, convertUrlToPdf);
+router.post('/convert/:uniqueId', authMiddleware, convertUrlToPdf);
 
 module.exports = router;
